44ab528c416a958dad83ce21a20495f5c143f879
Zdorov Filipp <zdorov-fk@zvuk.com>
2026-05-26 13:02:16 +0500

    Replace wget with curl in download_data script

--- download_data.sh
+++ download_data.sh
@@ -1,8 +1,10 @@
 BASE_URL=https://raw.githubusercontent.com/endless-sky/endless-sky/refs/heads/master/data/human/
 DATA_DIR=data/raw
 
-wget $BASE_URL/ships.txt -P $DATA_DIR
-wget $BASE_URL/weapons.txt -P $DATA_DIR
-wget $BASE_URL/power.txt -P $DATA_DIR
-wget $BASE_URL/outfits.txt -P $DATA_DIR
-wget $BASE_URL/engines.txt -P $DATA_DIR
+mkdir -p $DATA_DIR
+
+curl -sL "$BASE_URL/ships.txt" -o "$DATA_DIR/ships.txt"
+curl -sL "$BASE_URL/weapons.txt" -o "$DATA_DIR/weapons.txt"
+curl -sL "$BASE_URL/power.txt" -o "$DATA_DIR/power.txt"
+curl -sL "$BASE_URL/outfits.txt" -o "$DATA_DIR/outfits.txt"
+curl -sL "$BASE_URL/engines.txt" -o "$DATA_DIR/engines.txt"
